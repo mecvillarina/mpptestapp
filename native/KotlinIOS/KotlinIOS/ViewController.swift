8d1898f9ed765be7c9d9b200fbfe0085f4def0c8
Mark Erwin <mecodes_macmini@Marks-Mac-mini.local>
2020-04-20 17:52:52 +0800

    Added

---
+++ native/KotlinIOS/KotlinIOS/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  KotlinIOS
+//
+//  Created by Mark Erwin on 4/20/20.
+//  Copyright © 2020 Mark Erwin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
